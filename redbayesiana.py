2788af257ca99fe2cbf269974a0dd9d5d0614b9a
Raul Garcia <rauld.garcia95@gmail.com>
2023-06-20 21:26:35 -0600

    Codigo final

--- redbayesiana.py
+++ redbayesiana.py
@@ -3,7 +3,6 @@ import pandas as pd
 import math
 import time
 from itertools import combinations
-#import pyAgrum as gum
 
 inicio=time.time()
 
@@ -45,7 +44,7 @@ alpha={1 : 3.841,
        90 : 113.145,
        100 : 124.342}
 
-data=pd.read_csv('discretizadaCAIMwine.csv')
+data=pd.read_csv('discretizadaCAIMiris.csv')
 print(data)
 print(data.dtypes)
 lendata=len(data)
@@ -128,7 +127,7 @@ print("Total de atributos:")
 print(natributos)
 combinaciones=[]
 atributos=[]
-print("Calculando información mutua")
+print("\nCalculando información mutua")
 for combinacion in combinaciones2:
     i=mutual_info(combinacion[0],combinacion[1])
     t=2*lendata*i
@@ -152,7 +151,6 @@ for combinacion in combinaciones2:
     else:
         sl=alpha[100]    
     if t >= sl:
-        #print("Conecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
         combinaciones.append(combinacion)
         atributos.append(combinacion[0])
         atributos.append(combinacion[1])
@@ -169,41 +167,46 @@ atributos2=[]
 for combinacion in combinaciones:
     contsi=1
     contno=0
-    for atributo in atributos:
-        if not (atributo in combinacion):
-            i=conditional_mutual_info(combinacion[0],combinacion[1],atributo)
-            t=2*lendata*i
-            df=(len(list(set(list(data[combinacion[0]]))))-1)*(len(list(set(list(data[combinacion[1]]))))-1)*(len(list(set(list(data[atributo])))))
-            if df<=30:
-                sl=alpha[df]
-            elif df>30 and df <=40:
-                sl=alpha[30]
-            elif df>40 and df <=50:
-                sl=alpha[40]
-            elif df>50 and df <=60:
-                sl=alpha[50]
-            elif df>60 and df <=70:
-                sl=alpha[60]
-            elif df>70 and df <=80:
-                sl=alpha[70]
-            elif df>80 and df <=90:
-                sl=alpha[80]
-            elif df>90 and df <=100:
-                sl=alpha[90]
-            else:
-                sl=alpha[100] 
 
-            if t >= sl:
-                contsi+=1
-            else:
-                contno+=1
-
-    if contsi>=contno:
-        #print("Desconecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
-    #else:
+    if names[-1] in combinacion:
         combinaciones3.append(combinacion)
         atributos2.append(combinacion[0])
         atributos2.append(combinacion[1])
+
+    else:
+        for atributo in atributos:
+            if not (atributo in combinacion):
+                i=conditional_mutual_info(combinacion[0],combinacion[1],atributo)
+                t=2*lendata*i
+                df=(len(list(set(list(data[combinacion[0]]))))-1)*(len(list(set(list(data[combinacion[1]]))))-1)*(len(list(set(list(data[atributo])))))
+                if df<=30:
+                    sl=alpha[df]
+                elif df>30 and df <=40:
+                    sl=alpha[30]
+                elif df>40 and df <=50:
+                    sl=alpha[40]
+                elif df>50 and df <=60:
+                    sl=alpha[50]
+                elif df>60 and df <=70:
+                    sl=alpha[60]
+                elif df>70 and df <=80:
+                    sl=alpha[70]
+                elif df>80 and df <=90:
+                    sl=alpha[80]
+                elif df>90 and df <=100:
+                    sl=alpha[90]
+                else:
+                    sl=alpha[100] 
+
+                if t >= sl:
+                    contsi+=1
+                else:
+                    contno+=1
+
+        if contsi>=contno:
+            combinaciones3.append(combinacion)
+            atributos2.append(combinacion[0])
+            atributos2.append(combinacion[1])
 
 atributos2=list(set(atributos2))
 print("Combinaciones:")
@@ -214,64 +217,55 @@ print("\nCalculando información mutua condicional con dos atributos")
 
 combinaciones4=list(combinations(atributos2,2)) #combinaciones de 2 atributos con dos condicionales
 combinaciones5=[]
+atributosfin=[]
 for combinacion in combinaciones3:
     contsi=1
     contno=0
-    for combi in combinaciones4:
-        if not (combi[0] in combinacion) and not (combi[1] in combinacion):
-            i=conditional_mutual_info2(combinacion[0],combinacion[1],combi[0],combi[1])
-            t=2*lendata*i
-
-            df=(len(list(set(list(data[combinacion[0]]))))-1)*(len(list(set(list(data[combinacion[1]]))))-1)*(len(list(set(list(data[combi[0]])))))*(len(list(set(list(data[combi[1]])))))
-            if df<=30:
-                sl=alpha[df]
-            elif df>30 and df <=40:
-                sl=alpha[30]
-            elif df>40 and df <=50:
-                sl=alpha[40]
-            elif df>50 and df <=60:
-                sl=alpha[50]
-            elif df>60 and df <=70:
-                sl=alpha[60]
-            elif df>70 and df <=80:
-                sl=alpha[70]
-            elif df>80 and df <=90:
-                sl=alpha[80]
-            elif df>90 and df <=100:
-                sl=alpha[90]
-            else:
-                sl=alpha[100]    
-
-            if t >= sl:
-                contsi+=1
-            else:
-                contno+=1
-
-    if contsi>=contno:
-        #print("Desconecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
-    #else:
+    if names[-1] in combinacion:
         combinaciones5.append(combinacion)
-        #atributos2.append(combinacion[0])
-        #atributos2.append(combinacion[1])
+        atributosfin.append(combinacion[0])
+        atributosfin.append(combinacion[1])
 
-        """if t < sl and not combinacion in combinacioneseliminadas:
-                print("Desconecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
-                if bn.existsArc(combinacion[0],combinacion[1]):
-                    bn.eraseArc(combinacion[0],combinacion[1])
-                if combinacion in combinaciones5:
-                    combinaciones5.remove(combinacion)
-                combinacioneseliminadas.append(combinacion)
-            elif (combinacion in combinacioneseliminadas) and not (combinacion in combinaciones5): #si adelante sale que tiene que ir conectada la vuelve a conectar
-                print("Conecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
-                if not bn.existsArc(combinacion[0],combinacion[1]):
-                    bn.addArc(combinacion[0],combinacion[1])
-                combinaciones5.append(combinacion)
-                combinacioneseliminadas.remove(combinacion)
-                
-            #else: # si no hay que desconectar o reconectar solo lo agrega a la lista"""
+    else:
+        for combi in combinaciones4:
+            if not (combi[0] in combinacion) and not (combi[1] in combinacion):
+                i=conditional_mutual_info2(combinacion[0],combinacion[1],combi[0],combi[1])
+                t=2*lendata*i
 
+                df=(len(list(set(list(data[combinacion[0]]))))-1)*(len(list(set(list(data[combinacion[1]]))))-1)*(len(list(set(list(data[combi[0]])))))*(len(list(set(list(data[combi[1]])))))
+                if df<=30:
+                    sl=alpha[df]
+                elif df>30 and df <=40:
+                    sl=alpha[30]
+                elif df>40 and df <=50:
+                    sl=alpha[40]
+                elif df>50 and df <=60:
+                    sl=alpha[50]
+                elif df>60 and df <=70:
+                    sl=alpha[60]
+                elif df>70 and df <=80:
+                    sl=alpha[70]
+                elif df>80 and df <=90:
+                    sl=alpha[80]
+                elif df>90 and df <=100:
+                    sl=alpha[90]
+                else:
+                    sl=alpha[100]    
+
+                if t >= sl:
+                    contsi+=1
+                else:
+                    contno+=1
+
+        if contsi>=contno:
+            combinaciones5.append(combinacion)
+            atributosfin.append(combinacion[0])
+            atributosfin.append(combinacion[1])
+
+print("Combinaciones:")
 print(len(combinaciones5))
-print()
+print("Atributos:")
+print(len(list(set(atributosfin))))
 for combinacion in combinaciones5:
     print("Conecta el atributo ", combinacion[0], " con el atributo ", combinacion[1], ".")
 
